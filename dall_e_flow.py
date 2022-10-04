20c18cef64f8e6368a47ba29d1d4c47bd6c8d653
parker-g <94263482+parker-g@users.noreply.github.com>
2022-10-03 22:26:58 -0400

    dall e => now function

--- dall_e_flow.py
+++ dall_e_flow.py
@@ -3,9 +3,10 @@ from docarray import Document
 server_url = 'grpcs://dalle-flow.dev.jina.ai'
 def get_image(args:str):
     doc = Document(text=args).post(server_url, parameters={'num_images': 2})
-    return doc
-doc = get_image('blue sharks snack on giant hot dogs')
-da = doc.matches
-da.plot_image_sprites(fig_size=(10,10), show_index=True)
-
+    da = doc.matches
+    return da
+# da.plot_image_sprites(fig_size=(10,10), show_index=True)
+results = get_image('swirly clouds with a red sky')
+# results.plot_image_sprites(show_index=True)
+print(results[0])
     
