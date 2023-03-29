e1d19bb089bbbf55f7298a5e6c912813e4cedae3
parker-g <94263482+parker-g@users.noreply.github.com>
2023-03-29 10:16:53 -0400

    being to implement money feature

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,30 @@
 import random
+import pandas as pd
 from discord.ext.commands.cog import Cog
+from discord.ext import commands
+from discord import Member
+from config.config import BANK_PATH
+
+
+class Economy(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def withdraw_money(self, member:Member, money):
+        bank_df = pd.read_csv(BANK_PATH, header="infer")
+        users = self.bank_df.Username
+        # if member isn't in dataframe already, put them in and give them 100 GleepCoins
+        if member.name not in users:
+            bank_df.loc[len(bank_df.index)] = [member.name, 100]
+        current_balance = bank_df.loc[member.name, 'GleepCoins']
+        bank_df.loc[member.name, "GleepCoins"] = current_balance - money
+        bank_df.to_csv(BANK_PATH, index=False)
+        
+
+
+
+
 class Deck:
     color_legend = {
         "spades": "black",
@@ -43,8 +68,9 @@ class Deck:
         random.shuffle(self.deck)
 
 
-class Player:
-    def __init__(self, name):
+class Player(Cog):
+    def __init__(self, bot, name):
+        self.bot = bot
         self.name = name
         self.hand = []
         self.bust = False
@@ -54,6 +80,9 @@ class Player:
             self.done = True
         self.winner = False
 
+    @commands.command()    
+    async def initialize_player(self, bot):
+        pass
 
     def sumCards(self):
         total = 0
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 import get_assignments
 # created this file to test requesting an image from url, opening it,
 # saving it, and returning image url
@@ -10,8 +11,18 @@ import get_assignments
 #     with open(destination_url, 'wb') as file:
 #         file.write(response.content)
 #     return destination_url
-
+path = "data/bank.csv"
 # url = img_test0()
+test = {
+    "BagelBrigadier": 100,
+    "Goolie": 101, 
+    "Tom": 50000,
+}
+test = pd.read_csv("data/bank.csv", header='infer')
+test.to_csv(path, index=False)
+usernames = test.Username
 
-differences, time_diff = get_assignments.get_differences("data/last_time.txt")
-print(differences, time_diff)
+print(test)
+
+for user in usernames:
+    print(user)
