ff184b5ff010053342b4958224a50a5663d31c52
parker-g <94263482+parker-g@users.noreply.github.com>
2023-04-26 02:30:37 -0400

    create run script ( unfinished)
    thinking I can build this script to help new users easily create + activate a virtual environment, install requirements, create a config/config.py (maybe change to a yaml/toml format) and then finally, boot up the discord bot

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# test script
+activate() {
+    source .venv/Scripts/activate
+}
+
+WORKING_DIRECTORY=$(pwd) # /c/Users/rober/Documents/GitHub/ParkBot
+echo $WORKING_DIRECTORY
+activate()
