ddb2a4567382f7d8ec86a2e91019623e076ff480
parker g <94263482+parker-g@users.noreply.github.com>
2023-09-08 01:39:53 -0400

    add ffmpeg download + extraction
    need to:
    - write final destination of ffmpeg.exe to the config file
    - change the current config.py to an INI style 'config' file. i want to use toml but its a feature of only the latest and greatest version of python so I would be limiting my audience.
    - also need to read the config file for an existing 'ffmpeg.exe' location in the config + check that location in the filesystem for the ffmpeg executable

--- config/external-dep-setup.py
+++ config/external-dep-setup.py
@@ -4,6 +4,9 @@ import os
 import platform
 from pathlib import Path
 from enum import Enum
+from zipfile import ZipFile
+from tarfile import TarFile
+import configparser
 
 systems = {
     "linux": "linux",
@@ -19,6 +22,10 @@ machines = {
     "armv8b": "arm64",
     "armv8l": "arm64",
 }
+machines_to_github_versions = {
+    "64": "AMD64",
+    "arm64": "arm64",
+}
 
 class OSHome(Enum):
     windows = Path("C:/Users/")
@@ -29,15 +36,37 @@ class FFMPEG(Enum):
     linux = "ffmpeg"
 
 class FFMPEGDownloader:
-    """Parses different versions of FFMPEG."""
+    """Downloads + extracts FFMPEG archive based on user OS and CPU architecture."""
     
-    #NOTE this host only has linux 64 bit, linux ARM 64 bit, and win64 bit builds
-    #NOTE prioritize LGPL builds (GPL builds require works that use them to also be licensed under GPL)
-    
-    #NOTE perhaps check the user's architecture BEFORE grabbing the builds page. that way I can look for the user's proper build 
-    # based on their arc
+    #thanks stackoverflow
+    def downloadURL(self, url, save_path, chunk_size=128) -> None:
+        r = requests.get(url, stream=True)
+        with open(save_path, 'wb') as fd:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                fd.write(chunk)
+
+    def extract(self, archive, extract_destination) -> None:
+        """Extracts `archive` into `extract_destination`."""
+        match self.operating_sys:
+            case "linux":
+                self.extractTar(archive, extract_destination)
+            case "windows":
+                self.extractZip(archive, extract_destination)
+        
+    def extractZip(self, zip_file, unzip_destination) -> None:
+        zip = ZipFile(zip_file, "r")
+        zip.extractall(unzip_destination)
+
+    def extractTar(self, tar_file, extract_destination) -> None:
+        tar = TarFile(tar_file, "r")
+        tar.extractall(extract_destination)
+
+    # linux64-gpl-shared-tar, https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl-shared.tar.xz
+    # linux64-gpl-tar, https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
+    # linux64-lgpl-shared-tar, https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-lgpl-shared.tar.xz
 
     def getBuilds(self) -> dict[str, str]:
+        host = "https://github.com"
         """GET requests a github repo that hosts FFMPEG builds, and parses response's contents to return a dictionary of {'build_name':'build_download_link'}"""
         response = requests.get("https://github.com/BtbN/FFmpeg-Builds/releases")
         soup = bs4.BeautifulSoup(response.content, "html.parser")
@@ -45,47 +74,68 @@ class FFMPEGDownloader:
         results = {}
         for item in list_items:
             title = item.find("span", attrs={"class":"Truncate-text text-bold"})
-            if title is not None:
-                tit = title.text
-            link = item.find("a", attrs={"class":"Truncate"})
-            if link is not None:
-                final_link = link["href"]
-            results[tit] = final_link
+            if (title is not None) and (title.text != "Source code") and (title.text != ""):
+                print(title.text)
+                words = title.text.split("-")
+                words = words[:-1] + words[-1].split(".")[:2]
+                tit = ("-".join(words[3:]))
+                link = item.find("a", attrs={"class":"Truncate"})
+                link = link["href"]
+                results[tit] = host + link
         return results
     
-    def getBestBuild(self, operating_system):
-    
-    def downloadFFMPEG(self, destination:Path) -> None:
-        pass
-
-    def __init__(self, os:str):
+    def __init__(self, os:str, machine:str, download_destination = None):
         self.operating_sys = os
+        self.machine = machine
         self.builds = self.getBuilds()
-        # download proper version then download NSSM
-        self.best_match = ""
+        # {
+        # linux64-gpl-shared-tar
+        # linux64-gpl-tar
+        # linux64-lgpl-shared-tar
+        # linux64-lgpl-tar
+        # linuxarm64-gpl-shared-tar
+        # linuxarm64-gpl-tar
+        # linuxarm64-lgpl-shared-tar
+        # linuxarm64-lgpl-tar
+        # win64-gpl-shared-zip
+        # win64-gpl-zip 
+        # }
 
+        self.best_match = None
+        self.download_destination = download_destination
 
-    def getDownloadLink(self, platform:str) -> str:
+    def getBestLinuxBuildLink(self, builds) -> str:
+        "Gets download link for Linux depending on user's CPU instruction set."
+        match self.machine:
+            case "AMD64":
+                return builds["linux64-lgpl-tar"]
+            case "arm64":
+                return builds["linuxarm64-lgpl-tar"]
+            case _:
+                raise OSError(f"This computer's architecture is not currently supported for easy setup.")
+
+    def getDownloadLink(self, builds):
         """Returns the most appropriate FFMPEG download link for a user's operating system."""
+        match self.operating_sys:
+            case "windows":
+                return builds["win64-gpl-zip"]
+            case "linux":
+                return self.getBestLinuxBuildLink(builds)
+            case _:
+                raise OSError("This computer's architecture is not currently supported for easy setup.")
+            
 
-class HandlerFactory:
-    @classmethod
-    def getHandler(cls):
-        try:
-            operating_sys:str = platform.system().lower()
-            match operating_sys:
-                case "windows":
-                    return WindowsDependencyHandler()
-                case "linux":
-                    return LinuxDependencyHandler()
-        except KeyError:
-            raise OSError(f"This program does not currently support setup for operating systems besides linux or windows.")
-
+    def downloadFFMPEG(self, download_destination, extract_destination) -> None:
+        link = self.getDownloadLink(self.builds)
+        self.downloadURL(link, download_destination) # downloads zip
+        self.extract(download_destination, extract_destination)
+        
 
 class ExternalDependencyHandler:
-    """Abstract class to model DependencyHandler behaviors"""
+    """Super class to model DependencyHandler behaviors and provide basic operations."""
     def __init__(self):
         self.operating_sys = platform.system().lower()
+        self.machine = machines[platform.machine()]
 
     def isInProjectRoot(self) -> bool:
         """Checks if the terminal is in the ParkBot root directory."""
@@ -104,16 +154,16 @@ class ExternalDependencyHandler:
                 if desired_file in files:
                     return Path(root) / desired_file
         return None
-        
+
     def findFFMPEG(self, operating_system:str) -> Path | None:
         pass
 
-    def main(self):
+    def main(self, download_dest, extract_destination):
         ffmpeg = self.findFFMPEG(self.operating_sys)
         parkbot_scripts_dir = Path(os.getcwd()) / ".venv" / "Scripts"
         if not ffmpeg:
-            client = FFMPEGDownloader()
-            client.downloadFFMPEG(parkbot_scripts_dir) # download FFMPEG to the parkbot direectory
+            downloader = FFMPEGDownloader(self.operating_sys, self.machine)
+            downloader.downloadFFMPEG(parkbot_scripts_dir)
 
 class WindowsDependencyHandler(ExternalDependencyHandler):
     """Searches for and downloads depedencies specific to Windows."""
@@ -160,10 +210,18 @@ class LinuxDependencyHandler(ExternalDependencyHandler):
         dirs_to_search = [parkbot, self.user_home, usr_local, opt]
         return self.findFile(FFMPEG.linux.value, dirs_to_search)
 
-
-        
-class WindowsDownloader(FFMPEGDownloader):
-    def __init__(self, )
+class HandlerFactory:
+    @classmethod
+    def getHandler(cls):
+        try:
+            operating_sys:str = platform.system().lower()
+            match operating_sys:
+                case "windows":
+                    return WindowsDependencyHandler()
+                case "linux":
+                    return LinuxDependencyHandler()
+        except KeyError:
+            raise OSError(f"This program does not currently support setup for operating systems besides linux or windows.")
 
 
 if __name__ == "__main__":
