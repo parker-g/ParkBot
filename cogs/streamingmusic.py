1d08303f40e2031932cabb960c665bef95bc1326
parker g <94263482+parker-g@users.noreply.github.com>
2023-11-07 17:37:45 -0500

    working with new event
    - found out about the cog's 'cog_load' event, which happens just once whenever a cog is initially loaded from the bot. i'm using this event as the time for the cog to establish a connection to the lavalink server.
    
    - found out about the event, ''on_voice_state_update", which makes it much easier for me to implement an automatic-leaving function for the voice client. knowing which events exist is so useful lmao, it's drastically reduced the complexity of my code. also I have to hand it to the wavelink library for being pretty clear and easy to work with. I was in a way proud of the complexity of the last iteration of the music cog lol but now, I realize that it was so much harder to work with. I had a hard time pretty much having to re-teach myself how the music cog's complex conditionals execute every time I wanted/needed to make changes to it.
    - also changed many print statements to logger outputs.

--- cogs/streamingmusic.py
+++ cogs/streamingmusic.py
@@ -1,16 +1,20 @@
 from config.configuration import LAVALINK_URI, LAVALINK_PASS, WORKING_DIRECTORY
 from wavelink import Player, YouTubeTrack
+from discord.ext.commands.errors import ExtensionFailed
 from discord.ext.commands import Cog
 from discord.ext import commands
 from discord import Embed
 from pathlib import Path
 import wavelink
 import logging
-import aiohttp
-import io
 import asyncio
 import discord
 import time
+
+music_log_path = Path(WORKING_DIRECTORY) / "music.log"
+music_handler = logging.FileHandler(music_log_path, encoding="utf-8", mode="w")
+logger = logging.Logger("music_logger")
+logger.addHandler(music_handler)
 
 def getTime() -> str:
     return time.asctime(time.localtime())
@@ -20,6 +24,46 @@ class StreamingCog(Cog):
     def __init__(self, bot):
         self.bot = bot
         self.node = None
+
+    #TODO need to find how to clean this up and handle it accordingly when it fails
+    async def cog_load(self) -> None:
+        """Called when the bot loads this cog - sets up a connection to the lavalink server. Only necessary to do so once; a single node can serve multiple guilds at once."""
+        if self.node is None:
+            try:
+                node = wavelink.Node(uri=LAVALINK_URI, password=LAVALINK_PASS, retries=2)
+            except ExtensionFailed:
+                logger.error(f"Error connecting to lavalink server.")
+                return
+            nodes = await wavelink.NodePool.connect(client=self.bot, nodes=[node])
+            if nodes:
+                self.node = self.getNode()
+                logger.info(f"Node created + connected to lavalink server.")
+            else:
+                logger.error(f"Node created but couldn't establish connection to lavalink server.")
+        elif self.node is not None:
+            logger.info(f"A node connection has already been established.")
+
+
+    @commands.Cog.listener("on_voice_state_update")
+    async def leaveIfFinished(self, member:discord.Member, before:discord.VoiceState, after:discord.VoiceState):
+        """This method call acts as an occasional check up on the bot's voice client, disconnecting the bot if its not playing."""
+        if not member.id == self.bot.user.id:
+            return
+        elif before.channel is None:
+            if after.channel is not None:
+                voice:Player = after.channel.guild.voice_client
+                time = 0
+                while True:
+                    await asyncio.sleep(1)
+                    time = time + 1
+                    if voice.is_playing() and not voice.is_paused():
+                        time = 0
+                    if time == 600:
+                        await voice.disconnect()
+                        await after.channel.send(embed=Embed(title=f"Leaving voice chat due to inactivity."))
+                    if not voice.is_connected():
+                        break
+
 
     @commands.command()
     async def showNode(self, ctx) -> None:
@@ -48,16 +92,16 @@ class StreamingCog(Cog):
 
     @commands.command()
     async def createNode(self, ctx) -> None:
-        print("Connecting to lavalink node.")
+        logger.info("Connecting to lavalink node.")
         if self.node is None:
             node = wavelink.Node(uri=LAVALINK_URI, password=LAVALINK_PASS)
             nodes = await wavelink.NodePool.connect(client=self.bot, nodes=[node])
             if nodes:
                 self.node = self.getNode()
             else:
-                print(f"Error connecting to Lavalink Node")
+                logger.error(f"Error connecting to Lavalink Node")
         elif self.node is not None:
-            print(f"A node is already established and connected with ID: {self.node.id}")
+            logger.info(f"A node is already established and connected with ID: {self.node.id}")
         return
 
     #NOTE dont even need to explicitly get a node really, it seems the methods that require a node automatically grab one from the NodePool
@@ -150,22 +194,6 @@ class StreamingCog(Cog):
         if channel is not None:
             player = await channel.connect(cls= Player, timeout = 0)
             return player
-        # if channel is not None:
-        #     player = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-        #     if player is not None:
-        #             print(player)
-        #             if not player.is_playing():
-        #                 await player.disconnect()
-        #                 user_channel = ctx.author.voice.channel
-        #                 player = await user_channel.connect(cls = Player, timeout = None)
-        #             else:
-        #                 # player is playing already so leave it be
-        #                 pass
-
-        #     elif (player is None):
-        #         user_channel = ctx.author.voice.channel
-        #         player = await user_channel.connect(cls = Player, timeout = None)
-        #     return player
 
     @commands.command("showQ")
     async def showQueue(self, ctx) -> None:
@@ -226,28 +254,7 @@ class StreamingCog(Cog):
 
     # @commands.Cog.listener()
     # async def on_wavelink_track_end(node:wavelink.Node):
-        # need to check if the normal queue is empty after a track is over, and if so then stop playing songs. ( before autoplay populates queue)
-
-
-
-
-    # TODO on_ready event can be called multiple times in a discord session, so should implement
-    # logic here for checking if a node already exists before trying to connect to a new one
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print("Connecting to lavalink node.")
-        if self.node is None:
-            node = wavelink.Node(uri=LAVALINK_URI, password=LAVALINK_PASS)
-            nodes = await wavelink.NodePool.connect(client=self.bot, nodes=[node])
-            if nodes:
-                print(f"Node connected at ID: {nodes.keys()}")
-                self.node = self.getNode()
-            else:
-                print(f"Error connecting to Lavalink Node")
-        elif self.node is not None:
-            print(f"A node is already established and connected.")
-        return
+        #NOTE need to check if the normal queue is empty after a track is over, and if so then stop playing songs. ( before autoplay populates queue)
 
 
     @commands.Cog.listener()
