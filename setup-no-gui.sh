4a6e281be402dcd077021d8a2fdf3c334e1fac2b
parker g <94263482+parker-g@users.noreply.github.com>
2024-02-04 10:31:23 -0500

    bash script fails on linux
    - need to dynamically acquire the activation script from .venv instead of hard coding it's location
    
    - also linux testing fails during venv-setup. the reason seems to be that one of the pip requirements is pywin32 ( a windows only package) is included in my requirements.txt. I'm not sure what this is a dependency for so I'm trying to investigate if I can remove it before looking for another solution.

--- setup-no-gui.sh
+++ setup-no-gui.sh
@@ -37,6 +37,7 @@ fi
 
 system=$(uname)
 "$python_keyword" config/venv-setup.py # creates venv, installs pip dependencies
+# BUG - need to dynamically acquire the .venv activation script instead of hard coding it
 source .venv/Scripts/activate
 "$python_keyword" config/external-dep-setup.py # determines OS, checks for installation of FFMPEG, downloads FFMPEG. downloads NSSM and installs ParkBot as a Windows service if OS is Windows
 if [ ${system} = "Linux" ]; then
