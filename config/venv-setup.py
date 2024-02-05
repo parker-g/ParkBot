251018b845f1f8d4627f694f78853a5fe8ba89b2
parker g <94263482+parker-g@users.noreply.github.com>
2024-02-05 07:57:36 -0500

    is_env_created => agnostic
    also fixed naming conventions in venv setup

--- config/venv-setup.py
+++ config/venv-setup.py
@@ -6,7 +6,7 @@ from venv import EnvBuilder
 
 class VenvSetupWizard:
     
-    def isInProjectRoot(self) -> bool:
+    def is_in_project_root(self) -> bool:
         """Checks if the terminal is in the ParkBot root directory."""
         here = Path(os.getcwd())
         root_components = {"cogs", "data"}
@@ -16,7 +16,7 @@ class VenvSetupWizard:
             return True
         return False
 
-    def findPython(self) -> Path | None:
+    def find_python(self) -> Path | None:
         """Returns the path to the python executable in the `.venv` directory. Assumes it's being executed in the ParkBot root directory."""
         dir_to_search = Path(os.getcwd()) / ".venv"
         match platform.system().lower():
@@ -33,19 +33,25 @@ class VenvSetupWizard:
                     if os.access(python_exe, os.X_OK):
                         return Path(dirpath) / desired_file
     
-    def createVenv(self, project_root:Path) -> None:
+    def create_venv(self, project_root:Path) -> None:
         """Creates a virtual environment and saves the location of it's python executable."""
-        venv_exists = self.isEnvCreated()
+        venv_exists = self.is_env_created()
         if venv_exists is False:
             builder = EnvBuilder(with_pip=True)
             builder.create(project_root)
         #BUG need to make this python executable platform agnostic instead of hard coding it
-        os.environ["PARKBOT_PYTHON"] = str(self.findPython())
+        os.environ["PARKBOT_PYTHON"] = str(self.find_python())
 
-    def isEnvCreated(self) -> bool:
+    def is_env_created(self) -> bool:
         """Checks if a virtual environment subdirectory exists at the root level of ParkBot."""
         root_subdirs = [x.name for x in Path(os.getcwd()).iterdir() if x.is_dir()]
-        venv_components = {"lib", "scripts", "include"}
+        match platform.system().lower():
+            case "windows":
+                venv_components = {"lib", "scripts", "include"}
+            case "linux":
+                venv_components = {"lib", "bin", "include"}
+            case _:
+                raise OSError("This operating system is not supported by the venv-setup wizard.")
         for path in root_subdirs:
             path = Path(path)
             sub_sub_dirs = set([x.name.lower() for x in path.iterdir() if x.is_dir()])
@@ -55,12 +61,12 @@ class VenvSetupWizard:
                 return True
         return False
 
-    def pipInstallDeps(self) -> None:
+    def pip_install_deps(self) -> None:
         """Creates a subprocess to pip-install dependencies listed in requirements.txt."""
         subprocess.check_call([os.environ["PARKBOT_PYTHON"], "-m", "pip", "install", "-r", "requirements.txt"])
 
     def __init__(self):
-        inRoot = self.isInProjectRoot()
+        inRoot = self.is_in_project_root()
         if inRoot is False:
             raise FileNotFoundError(f"You must execute the setup wizard from the root directory of the ParkBot project.")
         self.root = Path(os.getcwd())
@@ -70,8 +76,8 @@ class VenvSetupWizard:
         self.threads = self.data_dir/ "threads.csv"
 
     def main(self):
-        self.createVenv(self.env)
-        self.pipInstallDeps()
+        self.create_venv(self.env)
+        self.pip_install_deps()
 
 if  __name__ == "__main__":
     wiz = VenvSetupWizard()
